cf130fd55a182a026c41c368c1d63f6e830348bf
cloud_123 <100378934+cloudd123@users.noreply.github.com>
2023-07-18 15:10:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// First Labour : Heracles vs Nemean Lion
+const Fighter = require('Fighter')
+// use your Fighter class here
+const heracles = new Fighter('🧔 Heracles',20,6);
+const nemeanLion = new Fighter('🦁 Nemean Lion', 11, 13); 
---
+++ src/Fighter.js
@@ -0,0 +1,10 @@
+/* Fighter class definition */
+const MAX_LIFE = 100;
+class Fighter{
+    constructor(name, strength, dexterity,life){
+        this.name = name;
+        this.strength = strength; 
+        this.dexterity = dexterity; 
+        MAX_LIFE = life; 
+    }
+}
